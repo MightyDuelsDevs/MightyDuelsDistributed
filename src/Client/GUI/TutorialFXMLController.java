2309d78cb279bbbd5a0cf69ac316c14f985232ca
Loek Delahaye <Loekdelahaye@gmail.com>
2015-11-30 22:18:55 +0100

    Merge origin/master
    Added a tutorial screen and included the background in the images.

--- src/Client/GUI/TutorialFXMLController.java
+++ src/Client/GUI/TutorialFXMLController.java
@@ -7,7 +7,9 @@ package Client.GUI;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -16,12 +18,25 @@ import javafx.fxml.Initializable;
  */
 public class TutorialFXMLController implements Initializable {
 
+    @FXML 
+    private ImageView Imv1 = new ImageView("@../Resources/Images/Tutorial%20Screen.png");
+    @FXML 
+    private ImageView Imv2 = new ImageView("@../Resources/Images/Tutorial%20Screen2.png");
+    @FXML 
+    private ImageView Imv3 = new ImageView("@../Resources/Images/Tutorial%20Screen3.png");
+    @FXML 
+    private ImageView Imv4 = new ImageView("@../Resources/Images/Tutorial%20Screen4.png");
+    
+  
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        Imv1.setOpacity(0);
+        Imv2.setOpacity(0);
+        Imv3.setOpacity(0);
+        Imv4.setOpacity(0);
     }    
     
 }
